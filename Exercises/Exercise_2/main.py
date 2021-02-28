13865f9ad1d99014c6ee0355f74cba94e785fbaf
NoeVG <noe-vg@outlook.com>
2021-02-28 01:02:03 -0600

    add Sol. Exercise 2

---
+++ Exercises/Exercise_2/main.py
@@ -0,0 +1,17 @@
+'''
+Exercise 2: Hello
+
+Stephenson, B., 2014. The Python workbook A Brief
+Introduction with Exercises and Solutions. New York
+Dordrecht London: Springer.
+'''
+
+def main():
+    name = ""
+    print("Please, Input your name: ")
+    name = input()
+    print("Hello",name)
+
+
+if __name__ == "__main__":
+    main()
